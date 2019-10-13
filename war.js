e9c660fd052baf15514c461e1581340bfbeece96
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2019-10-13 17:58:33 -0500

    Adding funcitonality to the buttons and adding in some game logic

--- war.js
+++ war.js
@@ -1,3 +1,42 @@
+let scores, card1, card2, winningScore, gamePlaying;
+
 function startGame() {
-  document.getElementsByClassName(winner).getElementsByTag
+  //setting the variables to initial values
+  scores = [0,0];
+  card1 = 0;
+  card2 = 0;
+  gamePlaying = true;
+  document.querySelector(".winner").textContent = "";
+  updateScores();
+  console.log(scores);
 }
+
+function updateScores() {
+  document.querySelector('.scoreNum1').textContent = scores[0];
+  document.querySelector('.scoreNum2').textContent = scores[1];
+}
+
+// Initializing the game on load
+startGame();
+
+document.querySelector('.btn-newGame').addEventListener('click', startGame);
+
+document.querySelector('.btn-deal').addEventListener('click', function() {
+  //Playing the game when the deal button is pressed
+  if (gamePlaying) {
+    card1 = Math.floor(Math.random() * 13) + 2;
+    card2 = Math.floor(Math.random() * 13) + 2;
+    console.log(card1);
+    console.log(card2);
+    if (card1 > card2) {
+      scores[0]++;
+    } else if (card1 < card2) {
+      scores[1]++;
+    }
+    //updating the card images
+    document.querySelector(".card1img").src = "./images/card" + card1 + ".png";
+    document.querySelector(".card2img").src = "./images/card" + card2 + ".png";
+    //updating the scores
+    updateScores();
+  }
+})
