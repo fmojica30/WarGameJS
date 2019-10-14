b97894f64e0d27de798f83af284af43c94449445
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2019-10-14 10:08:25 -0500

    Fixing the change in layout and declaring a winner and reset button

--- war.js
+++ war.js
@@ -6,7 +6,10 @@ function startGame() {
   card1 = 0;
   card2 = 0;
   gamePlaying = true;
-  document.querySelector(".winner").textContent = "";
+  winningScore = 10;
+  document.querySelector(".winHead1").textContent = "";
+  document.querySelector(".winHead2").textContent = "";
+  document.querySelector(".pWin").textContent = "";
   updateScores();
   console.log(scores);
 }
@@ -38,5 +41,18 @@ document.querySelector('.btn-deal').addEventListener('click', function() {
     document.querySelector(".card2img").src = "./images/card" + card2 + ".png";
     //updating the scores
     updateScores();
+
+    //Chekcing to see if the game has ended
+    if (scores[0] >= winningScore) {
+      document.querySelector(".winHead1").textContent = "Winner";
+      document.querySelector(".winHead2").textContent = "is";
+      document.querySelector(".pWin").textContent = "Player 1";
+      gamePlaying = false;
+    }else if (scores[1] >= winningScore){
+      document.querySelector(".winHead1").textContent = "Winner";
+      document.querySelector(".winHead2").textContent = "is";
+      document.querySelector(".pWin").textContent = "Player 2";
+      gamePlaying = false;
+    }
   }
 })
